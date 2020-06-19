1ceb09bf226879b270dd101677a660160d3e2ff7
Josh Waller <mdx.programs@gmail.com>
2020-06-19 15:36:49 -0400

    init

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+
+const tsConfig = { lib: ["es5", "es6", "dom"], target: "es5" };
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "umd",
+    name: "walt"
+  },
+  plugins: [typescript(tsConfig), terser()],
+};
